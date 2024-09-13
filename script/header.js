b58a683d3f92dc8db64a88f3cf53c43f6827b99a
K <rubbish@dbzmail.com>
2024-09-14 01:00:28 +0900

    fix

--- script/header.js
+++ script/header.js
@@ -24,7 +24,6 @@ function formatDateToText(date){
 	return `${date.getFullYear()}年 ` +
 	`${fillChars(String(date.getMonth() + 1), 2, "0")}月` +
 	`${fillChars(String(date.getDate()), 2, "0")}日 ` +
-	(window.innerWidth > 960 ? "" : "<br>") +
 	`${fillChars(String(date.getHours()), 2, "0")}:${fillChars(String(date.getMinutes()), 2, "0")}:${fillChars(String(date.getSeconds()), 2, "0")}`;
 }
 
